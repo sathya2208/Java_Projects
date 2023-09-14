fe6b633cd097fa6727c0b7a5c71158fd44323b9f
empur <empur@Purushotham>
2023-09-14 14:48:41 +0530

    addition program

---
+++ java_projects/src/day1/addition.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+	int a=100,b=46;	
+    System.out.println("Enter the values:");
+    System.out.println("the addition of"+(a+b));
+	}
+
+}
