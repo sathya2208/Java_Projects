f89bee8a614d1fb184dc0cb570b7e42e8c417417
empur <empur@Purushotham>
2023-09-22 09:26:57 +0530

    day 2 programs

---
+++ java_projects/src/day2/Addition.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class Addition {
+
+	public static void main(String[] args) {
+	int a=10,b=100,c;
+	c=a+b;
+	System.out.println("the addition of a="+a+" and b="+b+" is "+c);
+		
+	}
+
+}
---
+++ java_projects/src/day2/Addition1.java
@@ -0,0 +1,20 @@
+package day2;
+import java.util.Scanner;
+public class Addition1 {
+
+	
+
+	
+	
+	public static void main(String[] args)  {
+	int a,b,c;
+	System.out.println("enter the values");
+	Scanner s=new Scanner(System.in);
+	a=s.nextInt();
+	b=s.nextInt();
+	c=a+b;
+	System.out.println("the addition of a="+a+" and b="+b+" is "+c);
+	
+	}
+
+}
