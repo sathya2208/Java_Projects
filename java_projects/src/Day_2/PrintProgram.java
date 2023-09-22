875c87fbc6ce9b29d2810fbca0ebd9792e19a145
empur <empur@Purushotham>
2023-09-22 09:41:18 +0530

    day 2 programs

---
+++ java_projects/src/Day_2/PrintProgram.java
@@ -0,0 +1,12 @@
+package Day_2;
+
+public class PrintProgram {
+
+	public static void main(String[] args) {
+	   System.out.println("welcome to fullstack");
+	   System.out.println("java developer class");
+	   System.out.print("by sathya");
+
+	}
+
+}
