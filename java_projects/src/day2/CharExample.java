514a5192f9400d2f4af0d5faf24013d74fe7d298
empur <empur@Purushotham>
2023-09-23 13:57:07 +0530

    Character Example

---
+++ java_projects/src/day2/CharExample.java
@@ -0,0 +1,31 @@
+package day2;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+	
+
+			// assign single character literal
+		       char ch='a';
+		       System.out.println(ch);
+		    // assigning number to character
+		       char ch1=70;
+		       System.out.println(ch1);
+		    //unicode to char
+		       char var1='\u00A7';
+		       System.out.println(var1);
+		    //unicode representation
+		       char var2='\u20AC';
+		       System.out.println(var2);
+		    //ASCII code representation
+		       int a='A';
+		       System.out.println(a);
+		       
+		       
+		       
+			}
+
+		}
+
+
+	
